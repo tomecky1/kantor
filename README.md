859a8f2f1dbd7890c13b79920201ab631c521574
Tomasz Piotrowski <tpiotrowski007@gmail.com>
2023-02-23 14:57:37 +0100

    updated README and fixed script.js

--- README.md
+++ README.md
@@ -11,3 +11,8 @@ This my first simple currency converter still in development [IN-PROGRESS]
 - add other currencies
 - implement a real converter values for selected currency
 - other stuff (in future) 😁
+
+*technology used in the project includes:*
+1. HTML 5
+2. CSS 3
+3. JavaScript - ECMAScript22 (ES13)
--- js/script.js
+++ js/script.js
@@ -1,35 +1,36 @@
-console.log("Witaj szanowny developerze 😎");
+{
+  console.log("Witaj szanowny developerze 😎");
 
-const formElement = document.querySelector(".js-form");
-const amountElement = document.querySelector(".js-baseCurrency");
-const currencyElement = document.querySelector(".js-currency");
-const valueElement = document.querySelector(".js-newValue");
+  const formElement = document.querySelector(".js-form");
+  const valueElement = document.querySelector(".js-newValue");
 
-const calculateResult = (amount, currency) => {
-  const rateEUR = 4.4278;
-  const rateGBP = 4.9384;
-  const rateUSD = 3.9058;
-  switch (currency) {
-    case "EUR":
-      return amount / rateEUR;
+  const calculateResult = (amount, currency) => {
+    const rateEUR = 4.4278;
+    const rateGBP = 4.9384;
+    const rateUSD = 3.9058;
+    switch (currency) {
+      case "EUR":
+        return amount / rateEUR;
 
-    case "GBP":
-      return amount / rateGBP;
+      case "GBP":
+        return amount / rateGBP;
 
-    case "USD":
-      return amount / rateUSD;
-  }
-};
+      case "USD":
+        return amount / rateUSD;
+    }
+  };
 
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
+  formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const amountElement = document.querySelector(".js-baseCurrency");
+    const currencyElement = document.querySelector(".js-currency");
+    const amount = +amountElement.value;
+    const currency = currencyElement.value;
 
-  const amount = +amountElement.value;
-  const currency = currencyElement.value;
+    let result = calculateResult(amount, currency);
 
-  let result = calculateResult(amount, currency);
-
-  valueElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(
-    2
-  )} ${currency}</strong>`;
-});
+    valueElement.innerHTML = `${amount.toFixed(
+      2
+    )} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
+  });
+}
