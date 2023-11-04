7f391ecec700e430b0194b94d1839c3cdaa2549b
Tomasz Piotrowski <tpiotrowski007@gmail.com>
2023-11-04 01:09:02 +0100

    fixed entire logic in script.js

--- js/script.js
+++ js/script.js
@@ -1,51 +1,40 @@
 {
   console.log("Witaj szanowny developerze 😎");
 
-  const formElement = document.querySelector(".js-form");
+  
   const valueElement = document.querySelector(".js-newValue");
 
   const calculateResult = (amount, currency) => {
-    // const rateEUR = 4.6580;
-    // const rateGBP = 5.5010;
-    // const rateUSD = 3.8580;
-
-  const rateEUR = fetch("https://api.nbp.pl/api/exchangerates/rates/a/eur/?format=json")
-    .then((response) => response.json())
-    .then((data) => data.rates[0].mid)
-    .catch((error) => console.log(error));
-
-  const rateGBP = fetch("https://api.nbp.pl/api/exchangerates/rates/a/gbp/?format=json")
-    .then((response) => response.json())
-    .then((data) => data.rates[0].mid)
-    .catch((error) => console.log(error));
-
-  const rateUSD = fetch("https://api.nbp.pl/api/exchangerates/rates/a/usd/?format=json")
-    .then((response) => response.json())
-    .then((data) => data.rates[0].mid)
-    .catch((error) => console.log(error));
-
+    const rateEUR = 4.6580;
+    const rateGBP = 5.1123;
+    const rateUSD = 3.8580;
+    
     switch (currency) {
       case "EUR":
         return amount / rateEUR;
-
+        
       case "GBP":
-        return amount / rateGBP;
+        return amount / rateGBP;    
 
       case "USD":
         return amount / rateUSD;
     }
   };
-  
+  const init = () => {
+    const formElement = document.querySelector(".js-form");
+    formElement.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const amountElement = document.querySelector(".js-baseCurrency");
+      const currencyElement = document.querySelector(".js-currency");
+      const resultElement = document.querySelector(".js-result");
 
-  formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const amountElement = document.querySelector(".js-baseCurrency");
-    const currencyElement = document.querySelector(".js-currency");
-    const amount = +amountElement.value;
-    const currency = currencyElement.value;
+      const amount = +amountElement.value;
+      const currency = currencyElement.value;
 
-    let result = calculateResult(amount, currency);
+      const result = calculateResult(amount, currency);
 
-    valueElement.innerHTML = `${amount} PLN = <strong>${result} ${currency}</strong>`;
+    resultElement.innerHTML = `${amount} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
   });
+};
+  init();
 }
